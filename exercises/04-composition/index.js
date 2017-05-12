57a96dd699de3b9712e429cd6bee3aca0122b522
Sanjay Purswani <sanjsanj@hotmail.com>
2017-05-12 16:07:45 +0100

    exercise 04

--- exercises/04-composition/index.js
+++ exercises/04-composition/index.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React from 'react'
 
 class List extends React.Component {
-  // TODO write your component here
   render() {
-    return null;
+    const list = this.props.topics.map(function(topic) {
+      return (<li>{topic}</li>);
+    });
+
+    return (
+      <ul>{list}</ul>
+    )
   }
 }
 
 class Topics extends React.Component {
-  // TODO write your component here
   render() {
+    const topics_array = [
+      'Component',
+      'Props',
+      'Composition'
+    ]
+
     return (
       <div>
         <h4>Below is a list a of topics</h4>
-        {/* TODO */}
+        <List topics={topics_array} />
       </div>
-    );
+    )
   }
 }
 
-export default Topics;
+export default Topics
