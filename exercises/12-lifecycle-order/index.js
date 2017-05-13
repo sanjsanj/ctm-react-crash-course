571d04ea6bf1e61181ee4e3551a66e6db4ad8bd6
sanjsanj <sanjsanj@hotmail.com>
2017-05-13 10:25:21 +0100

    exercise 12

--- exercises/12-lifecycle-order/index.js
+++ exercises/12-lifecycle-order/index.js
@@ -3,18 +3,38 @@ import React from 'react';
 class Display extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      count: 0
+      count: 0,
     };
+  }
+  componentWillMount() {
+    console.log(1, 'componentWillMount');
+  }
+  componentDidMount() {
+    console.log(2, 'componentDidMount');
+    this.interval = setInterval(this.tick.bind(this), 100);
+  }
+  componentWillReceiveProps() {
+    console.log(3, 'componentWillReceiveProps');
+  }
+  shouldComponentUpdate() {
+    console.log(4, 'shouldComponentUpdate');
+    return true;
+  }
+  componentWillUpdate() {
+    console.log(5, 'componentWillUpdate');
+  }
+  componentDidUpdate() {
+    console.log(6, 'componentDidUpdate');
+  }
+  componentWillUnmount() {
+    console.log(7, 'componentWillUnmount');
+    clearInterval(this.interval);
   }
   tick() {
     this.setState({
-      count: this.state.count + 1
+      count: this.state.count + 1,
     });
-  }
-  componentDidMount() {
-    this.interval = setInterval(this.tick.bind(this), 100);
   }
   render() {
     return (
@@ -26,28 +46,32 @@ class Display extends React.Component {
   }
 }
 
-export default class extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       count: 0,
-      show: false
+      show: false,
     };
+  }
+  componentDidMount() {
+    this.tickInterval = setInterval(this.tick.bind(this), 1000);
+    this.tockInterval = setInterval(this.tock.bind(this), 500);
+  }
+  componentWillUnmount() {
+    clearInterval(this.tickInterval);
+    clearInterval(this.tockInterval);
   }
   tick() {
     this.setState({
       count: this.state.count + 1,
-      show: !this.state.show
+      show: !this.state.show,
     });
   }
   tock() {
     this.setState({
-      count: this.state.count + 1
+      count: this.state.count + 1,
     });
-  }
-  componentDidMount() {
-    setInterval(this.tick.bind(this), 1000);
-    setInterval(this.tock.bind(this), 500);
   }
   render() {
     const display = this.state.show ? (
@@ -61,3 +85,5 @@ export default class extends React.Component {
     );
   }
 }
+
+export default App;
